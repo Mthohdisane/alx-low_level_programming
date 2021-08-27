ced744008409e8590c27a638e7ac5c65eabd8b6b
Mthohdisane <mthobisimshika55@gmail.com>
2021-08-27 08:57:38 -0700

    if_else_while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print the letters of the alphabet in reverse
+ *
+ *
+ * Return 0
+ */
+
+int main(void)
+{
+	int i = 122;
+
+	while (i > 96)
+	{
+		putchar(i);
+		i--;
+	}
+	putchar(10);
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - print all possible combinations of single-digit numbers
+ *
+ *
+ * Return 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 48;
+
+	while (i < 58)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		i++;
+	}
+
+	putchar(10);
+
+	return (0);
+}
